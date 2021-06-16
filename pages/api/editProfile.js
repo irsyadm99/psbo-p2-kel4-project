8aea19da507b82afd9f929683cdeb7da4f38d08b
TobiAlbertino <tobialbertino@gmail.com>
2021-06-16 14:03:50 +0700

    data dummy

--- pages/api/editProfile.js
+++ pages/api/editProfile.js
@@ -2,64 +2,12 @@ import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default async function editProfileHandler(req, res) {
-  if (req.method === "PUT") {
-    // Process a POST request
-    try {
-      const {
-        deskripsi,
-        pekerjaan,
-        suku,
-        pendidikanTerakhir,
-        domisili,
-        statusPernikahan,
-        hobi,
-        karakterPositif,
-        karakterNegatif,
-        userId,
-      } = req.body;
-
-      const result = await prisma.user.update({
-        where: {
-          id: userId,
-        },
-        data: {
-          deskripsiDiri: deskripsi,
-          pekerjaan: pekerjaan,
-          suku: suku,
-          pendidikanTerakhir: pendidikanTerakhir,
-          domisili: domisili,
-          statusPernikahan: statusPernikahan,
-          hobi: hobi,
-          karakterPositif: karakterPositif,
-          karakterNegatif: karakterNegatif,
-        },
-      });
-      // res.json(result);
-      res.status(200).json({
-        status: "Successfully edited !",
-        result,
-      });
-    } catch (err) {
-      res.status(400).json({
-        message: "Something went wrong",
-        detailMessage: err.message,
-      });
-    }
-  } else {
-    // Handle any other HTTP method
-    return res.status(405).json({
-      message: "Method not allowed-SALAH WOY",
-      detailMessage: err.message,
-    });
-  }
-}
 // export default async function editProfileHandler(req, res) {
-//   if (req.method === "POST") {
+//   if (req.method === "PUT") {
 //     // Process a POST request
 //     try {
 //       const {
-//         deskripsiDiri,
+//         deskripsi,
 //         pekerjaan,
 //         suku,
 //         pendidikanTerakhir,
@@ -68,21 +16,23 @@ export default async function editProfileHandler(req, res) {
 //         hobi,
 //         karakterPositif,
 //         karakterNegatif,
-//         userId
+//         userId,
 //       } = req.body;
 
-//       const result = await prisma.user.create({
+//       const result = await prisma.user.update({
+//         where: {
+//           id: userId,
+//         },
 //         data: {
-//           deskripsiDiri,
-//           pekerjaan,
-//           suku,
-//           pendidikanTerakhir,
-//           domisili,
-//           statusPernikahan,
-//           hobi,
-//           karakterPositif,
-//           karakterNegatif,
-//           userId
+//           deskripsiDiri: deskripsi,
+//           pekerjaan: pekerjaan,
+//           suku: suku,
+//           pendidikanTerakhir: pendidikanTerakhir,
+//           domisili: domisili,
+//           statusPernikahan: statusPernikahan,
+//           hobi: hobi,
+//           karakterPositif: karakterPositif,
+//           karakterNegatif: karakterNegatif,
 //         },
 //       });
 //       // res.json(result);
@@ -104,3 +54,57 @@ export default async function editProfileHandler(req, res) {
 //     });
 //   }
 // }
+
+
+export default async function editProfileHandler(req, res) {
+  if (req.method === "POST") {
+    // Process a POST request
+    try {
+      const {
+        deskripsiDiri,
+        pekerjaan,
+        suku,
+        pendidikanTerakhir,
+        domisili,
+        statusPernikahan,
+        hobi,
+        karakterPositif,
+        karakterNegatif,
+        userId
+      } = req.body;
+
+      const result = await prisma.user.create({
+        data: {
+          deskripsiDiri,
+          pekerjaan,
+          suku,
+          pendidikanTerakhir,
+          domisili,
+          statusPernikahan,
+          hobi,
+          karakterPositif,
+          karakterNegatif,
+          user: {
+            connect: {id: userId},
+          },
+        },
+      });
+      // res.json(result);
+      res.status(200).json({
+        status: "Successfully edited !",
+        result,
+      });
+    } catch (err) {
+      res.status(400).json({
+        message: "Something went wrong",
+        detailMessage: err.message,
+      });
+    }
+  } else {
+    // Handle any other HTTP method
+    return res.status(405).json({
+      message: "Method not allowed-SALAH WOY",
+      detailMessage: err.message,
+    });
+  }
+}
